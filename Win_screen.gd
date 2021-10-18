f4070255d131c11828d906c0cbe73b2a3c23fb4e
Jasper <87291184+Jasper-0195551@users.noreply.github.com>
2021-10-18 12:50:41 +1100

    int
    fix the loses screen button

--- Win_screen.gd
+++ Win_screen.gd
@@ -1,6 +1,9 @@
 extends Control
 
-export (String, FILE) var Main_Menu
 func _ready():
-	for button in $HBoxContainer/VBoxContainer/Button_place.get_children():
+	for button in $"HBoxContainer/VBoxContainer/Button_place".get_children():
 		button.connect("pressed", self, "_on_Button_pressed", [button.scene_to_load])
+func _on_Button_pressed(scene_to_load):
+	print("Changing Scene...")
+	print(scene_to_load)
+	get_tree().change_scene(scene_to_load)
